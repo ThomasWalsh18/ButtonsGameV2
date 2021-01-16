343102b962f1fd03f1ef07b11ef645935d555a41
ThomasWalsh <tomwalsh18@icloud.com>
2021-01-16 17:59:10 +0000

    Initial Commit

---
+++ Assets/Scenes/NewEngineTest/Scripts/GameManger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class GameManger : MonoBehaviour
+{
+    /// <summary>
+    /// Use an event system so that I can manage all the events easy 
+    /// such as pressing the  wrong button, trigger wrong event which hides all rows par the start
+    /// and pressing the right button will look at the row number and reveal the next row
+    /// And lose game hide all and reveal replay button, then its really easy to add different kind of events
+    /// </summary>
+    public static GameManger current;
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public event Action timerComplete;
+    public void outOfTime()
+    {
+        if (timerComplete != null)
+        {
+            timerComplete();
+        }
+    }
+
+}
---
+++ Assets/Scenes/NewEngineTest/Scripts/Timer.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public float totalTime = 60;
+    public int speed = 1;
+    public float delayAmount = 1.0f;
+    public bool count = true;
+
+    private Slider timer;
+    private float time = 0f;
+    void Start()
+    {
+        GameManger.current.timerComplete += CompleteTimer;
+        timer = gameObject.GetComponent<Slider>();
+        timer.maxValue = totalTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (count)
+        {
+            time = Mathf.Lerp(0, 1, Time.deltaTime * (1 * speed));
+            if (time + timer.value <= totalTime)
+            {
+                timer.value = timer.value + time;
+                timer.maxValue = totalTime;
+            }
+            else
+            {
+                GameManger.current.outOfTime();
+                count = false;
+            }
+        }
+    }
+
+    private void CompleteTimer()
+    {
+        print("Timer Complete");
+    }
+}
